a1a82eabae1ada6faffc252452b173070cc12876
Jobayer Hossain <jobayerhossain977@Gmail.com>
2020-10-28 21:08:17 +0600

    Re-Initialized app useing Expo CLI

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {View, Text} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-const App = () => {
+export default function App() {
   return (
-    <View>
-      <Text>hello</Text>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
